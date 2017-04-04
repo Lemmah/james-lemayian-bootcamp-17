8108e66a2436219f815bff5006f3cf6128e8d2b3
Lemmah <lemayiannakolah@gmail.com>
2017-04-04 10:24:02 +0300

    Creating a test_prime_numbers for my prime_numbers generator

---
+++ Day 1/PrimeNumbers/test_prime_numbers.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import unittest
+from prime_numbers import generate_primes
+
+class test_prime_numbers(unittest.TestCase):
+    def test_for_correct_output(self):
+        self.assertEquals(generate_primes(15),[1,2,3,5,7,11,13])
