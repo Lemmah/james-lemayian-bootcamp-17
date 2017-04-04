b286d8f814a33d7876cb5f25e03e18f1e404b52a
Lemmah <lemayiannakolah@gmail.com>
2017-04-04 10:33:23 +0300

    Two tests created
    The function has to pass the two tests:
    It should output a list of correct primes
    It should not accept any other input that is not an integer

--- Day 1/PrimeNumbers/test_prime_numbers.py
+++ Day 1/PrimeNumbers/test_prime_numbers.py
@@ -6,6 +6,6 @@ from prime_numbers import generate_primes
 
 class test_prime_numbers(unittest.TestCase):
     def test_for_number_input(self):
-        self.assertEquals(generate_primes('w22', 'Please input a valid number!'))
+        self.assertEquals(generate_primes('w22'), 'Please input a valid number!')
     def test_for_correct_output(self):
         self.assertEquals(generate_primes(15),[1,2,3,5,7,11,13])
