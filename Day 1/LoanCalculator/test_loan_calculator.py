08ec57da5b21ed60ac053a525186e40bddf5f1be
Lemmah <lemayiannakolah@gmail.com>
2017-04-04 08:37:07 +0300

    Reorganized my tests
    I reorganized my tests for easier sequential implementation in my function. I also added a new test to assert the input value type.

--- Day 1/LoanCalculator/test_loan_calculator.py
+++ Day 1/LoanCalculator/test_loan_calculator.py
@@ -15,15 +15,17 @@ import unittest
 from loan_calculator import calculate_loan_repayable
 
 class test_loan_calculator(unittest.TestCase): # Does it calculate the loan correctly?
-    def test_repayable_amount_output(self):
-        self.assertEquals(calculate_loan_repayable(100000,12,12), 112000)
+    def test_input_is_number(self):
+        self.assertEquals(calculate_loan_repayable("SELECT * FROM",8,2), "Input a valid number.")
+    def test_input_is_positive(self):
+        self.assertEquals(calculate_loan_repayable(10000,-1,-1), "You cannot have values less than 0.")
     def test_for_number_of_months(self): # It makes sure that the number of months are less than 12 as agreed
         self.assertEquals(calculate_loan_repayable(100000,13,11), "Maximum time allowed is 12 months.")
     def test_minimum_amount_borrowable(self):
-        self.assertEquals(calculate_loan_repayable(4600,3,13), "Minimum borrowable amount is 5000.")
-    def tes_maximum_amount_borrowable(self):
-        self.assertEquals(calculate_loan_repayable(1000000000,2,9), "You can only borrow upto a maximum of 10M.")
-    def test_input_is_positive(self):
-        self.assertEquals(calculate_loan_repayable(10000,-1,-1), "You cannot have values less than 0.")
+        self.assertEquals(calculate_loan_repayable(4600,3,13), "You can only borrow between 5000 and 10000000.")
+    def test_maximum_amount_borrowable(self):
+        self.assertEquals(calculate_loan_repayable(1000000000,2,9), "You can only borrow between 5000 and 10000000.")
+    def test_repayable_amount_output(self):
+        self.assertEquals(calculate_loan_repayable(100000,12,12), 112000)
 if __name__ == '__main__':
     unittest.main()
