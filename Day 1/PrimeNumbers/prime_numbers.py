6c62f59cdb76e0cd3cb49797b15880e13038232c
Lemmah <lemayiannakolah@gmail.com>
2017-04-04 10:33:43 +0300

    Function passes all the tests

---
+++ Day 1/PrimeNumbers/prime_numbers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+def generate_primes(n):
+    if isinstance(n, int):
+        primes_list = list()
+        for number in range(1,n+1):
+            prime = True
+            for i in range(2, number):
+                if (number % i == 0):
+                    prime = False
+            if prime:
+                primes_list.append(number)
+        return primes_list
+    else:
+        return "Please input a valid number!"
