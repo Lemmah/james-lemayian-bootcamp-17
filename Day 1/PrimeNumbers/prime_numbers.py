9e70213f179e8fbc93afecba83accce481310e3c
Lemmah <lemayiannakolah@gmail.com>
2017-04-06 02:11:19 +0300

    Pass more tests (5 tests)
    I had to modify the function to pass five the five tests

--- Day 1/PrimeNumbers/prime_numbers.py
+++ Day 1/PrimeNumbers/prime_numbers.py
@@ -3,14 +3,20 @@
 
 def generate_primes(n):
     if isinstance(n, int):
-        primes_list = list()
-        for number in range(2,n+1):
-            prime = True
-            for i in range(2, number):
-                if (number % i == 0):
-                    prime = False
-            if prime:
-                primes_list.append(number)
-        return primes_list
+        if n > 0:
+            if n > 1:
+                primes_list = list()
+                for number in range(2,n+1):
+                    prime = True
+                    for i in range(2, number):
+                        if (number % i == 0):
+                            prime = False
+                    if prime:
+                        primes_list.append(number)
+                return primes_list
+            else:
+                return 'Please input a number greater than 1.'
+        else:
+            return 'Please input a positive number.'
     else:
-        return "Please input a valid number!"
+        return 'Please input a valid number!'
