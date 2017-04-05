4772f3b1024b65c1a86314f6bbd57e139fcc0ca0
Lemmah <lemayiannakolah@gmail.com>
2017-04-06 02:10:25 +0300

    Prime numbers tests added up to five tests
    I had misunderstood the question and made just two test which I thought were adequate.
    But now I got the five required ones

--- Day 1/PrimeNumbers/test_prime_numbers.py
+++ Day 1/PrimeNumbers/test_prime_numbers.py
@@ -8,4 +8,10 @@ class test_prime_numbers(unittest.TestCase):
     def test_for_number_input(self):
         self.assertEquals(generate_primes('w22'), 'Please input a valid number!')
     def test_for_correct_output(self):
-        self.assertEquals(generate_primes(15),[1,2,3,5,7,11,13])
+        self.assertEquals(generate_primes(15),[2,3,5,7,11,13])
+    def test_for_negative_input(self):
+    	self.assertEquals(generate_primes(-2), 'Please input a positive number.')
+    def test_for_not_zero(self):
+    	self.assertEquals(generate_primes(0), 'Please input a positive number.')
+    def test_for_not_one(self):
+    	self.assertEquals(generate_primes(1), 'Please input a number greater than 1.')
