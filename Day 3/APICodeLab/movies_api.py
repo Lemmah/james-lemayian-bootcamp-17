409e3cfe5e2667bf13f5b28bb9af653ccb1043ee
Lemmah <lemayiannakolah@gmail.com>
2017-04-06 22:59:44 +0300

    Corrections to the malfunctioning API

--- Day 3/APICodeLab/movies_api.py
+++ Day 3/APICodeLab/movies_api.py
@@ -1,11 +1,12 @@
 import requests
+import json
 
 def consume_api():
-	movie_name = raw_input('Enter movie name to search: \n')
-	my_api_key = 'ee58a870044d3b4369b90a9b1a906e57'
+	movie_query = raw_input('Enter movie name to search: \n')
+	api_key = 'ee58a870044d3b4369b90a9b1a906e57'
 
-	api_response = requests.get('https://api.themoviedb.org/3/movie/550?api_key=ee58a870044d3b4369b90a9b1a906e57')
+	api_response = requests.get('https://api.themoviedb.org/3/search/movie?api_key=%s&language=en-US&query=%s&page=1&include_adult=false' % (api_key,movie_query))
 
 	print('\n These are the movies results: \n')
 	print(api_response.json())
-consume_api()
+consume_api()	
