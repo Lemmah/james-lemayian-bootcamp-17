0ad70b67a0aad44455f10f2ee0346b45a693316e
Lemmah <lemayiannakolah@gmail.com>
2017-04-06 23:08:07 +0300

    Modified the function name for easier understanding2

--- Day 3/APICodeLab/movies_api.py
+++ Day 3/APICodeLab/movies_api.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
-def consume_api():
+def moviedb_api():
 	movie_query = raw_input('Enter movie name to search: \n')
 	api_key = 'ee58a870044d3b4369b90a9b1a906e57'
 
 	api_response = requests.get('https://api.themoviedb.org/3/search/movie?api_key=%s&language=en-US&query=%s&page=1&include_adult=false' % (api_key,movie_query))
-
+	movies_json = api_response.json()
 	print('\n These are the movies results: \n')
-	print(api_response.json())
-consume_api()	
+	print(movies_json)
+moviedb_api()	
